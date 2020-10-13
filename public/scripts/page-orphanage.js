3c7caed25e27c3c6402a7397b2419d77610f9839
Milena <milenaalmeidadev@hotmail.com>
2020-10-13 16:01:53 -0400

    Olhando as oportunidades

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,51 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+};
+
+//create map
+const map = L.map('mapid', options).setView([-27.222633, -49.6455874], 15);
+
+// create and add tileLayes
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+// create icon
+const icon = L.icon({
+    iconUrl: './public/images/map-marker.svg',
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+});
+
+// create and add marker
+L
+.marker([-27.222633, -49.6455874], { icon })
+.addTo(map);
+
+/* image galarry */
+function selectImage(event){
+    const button = event.currentTarget;
+
+    console.log(button.children);
+
+    //remover todas as classes .active
+    const buttons = document.querySelectorAll('.images button');
+    buttons.forEach(removeActiveClass);
+
+    function removeActiveClass(button){
+        button.classList.remove('active');
+    }
+
+    //selecionar a imagem clicada
+    const image = button.children[0];
+    const imageContainer = document.querySelector('.orphanage-details > img');
+
+    //atualizar o container da imagem
+    imageContainer.src = image.src;
+
+    // adicionar a classe .active para este botão
+    button.classList.add('active');
+}

0d21e75115d183767e3196ed9cac10dea3138e41
Milena <milenaalmeidadev@hotmail.com>
2020-10-15 11:14:48 -0400

    Até 2 anos em 2 meses

---
+++ src/database/fakedata.js
@@ -0,0 +1,34 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-27.222633",
+        lng: "-49.6455874",
+        name: "Lar das meninas",
+        description: "Presta assitência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+
+            "https://images.unsplash.com/photo-1502086223501-7ea6ecd79368?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=723&q=80",
+
+            "https://images.unsplash.com/photo-1503454537195-1dcabb73ffb9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=333&q=80"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "1"
+    },
+    {
+        id: 2,
+        lat: "-27.222633",
+        lng: "-49.6555874",
+        name: "Lar dos meninos",
+        description: "Presta assitência a crianças de 02 a 18 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+
+            "https://images.unsplash.com/photo-1502086223501-7ea6ecd79368?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=723&q=80",
+
+            "https://images.unsplash.com/photo-1485546246426-74dc88dec4d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 7h até 20h",
+        open_on_weekends: "0"
+    }
+]
